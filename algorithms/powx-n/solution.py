dc00ea92e2bc513c992000289b277b6f3c5bd77f
Golden8922 <gky8922@gmail.com>
2026-09-19 02:05:48 +0530

    feat: add Pow(x, n) solution

--- algorithms/powx-n/solution.py
+++ algorithms/powx-n/solution.py
@@ -1,14 +1,4 @@
 class Solution:
-    def countCommas(self, n: int) -> int:
-        total = 0
-        start = 1000
-        commas = 1
-
-        while start <= n:
-            end = min(n, start * 1000 - 1)
-            total += (end - start + 1) * commas
-
-            start *= 1000
-            commas += 1
-
-        return total
+    def myPow(self, x: float, n: int) -> float:
+        return(x**n)
+        
